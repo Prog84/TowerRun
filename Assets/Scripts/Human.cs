1bec3a7bffe09b1c93703456e666ef83abded829
Prog84 <bvv198424@rambler.ru>
2021-02-08 17:47:41 +1000

    Add Tower and PlayerTower scripts

---
+++ Assets/Scripts/Human.cs
@@ -0,0 +1,8 @@
+﻿using UnityEngine;
+
+public class Human : MonoBehaviour
+{
+    [SerializeField] private Transform _fixationPoint;
+
+    public Transform FixationPoint => _fixationPoint;
+}
---
+++ Assets/Scripts/Jumper.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+public class Jumper : MonoBehaviour
+{
+    [SerializeField] private float _jumpForce;
+
+    private bool _isGrouned;
+    private Rigidbody _rigidbody;
+
+    private void Start()
+    {
+        _rigidbody = GetComponent<Rigidbody>();    
+    }
+
+    private void Update()
+    {
+        if (Input.GetMouseButtonDown(0) && _isGrouned)
+        {
+            _isGrouned = false;
+            _rigidbody.AddForce(Vector3.up * _jumpForce);
+        }        
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.TryGetComponent(out Road road))
+        {
+            _isGrouned = true;
+        }
+    }
+}
